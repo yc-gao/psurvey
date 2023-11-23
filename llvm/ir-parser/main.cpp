f5a1d1d77737cc57e2540a3f6e9f5314be88d94b
yinchaogao <yinchaogao@deeproute.ai>
2023-11-23 15:12:31 +0800

    pass ir-parser

--- llvm/ir-parser/main.cpp
+++ llvm/ir-parser/main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
 #include "llvm/IRReader/IRReader.h"
 #include "llvm/Support/CommandLine.h"
-#include "llvm/IR/LLVMContext.h"
 #include "llvm/Support/SourceMgr.h"
-#include "llvm/IR/Module.h"
 
-namespace
-{
-    llvm::cl::opt<std::string> InputFile(llvm::cl::Positional, llvm::cl::init("-"));
+namespace {
+llvm::cl::opt<std::string> InputFile(llvm::cl::Positional, llvm::cl::init("-"));
 }
 
-int main(int argc, char const *argv[])
-{
-    llvm::cl::ParseCommandLineOptions(argc, argv, "llvm ir reader");
+int main(int argc, char const *argv[]) {
+  llvm::cl::ParseCommandLineOptions(argc, argv, "llvm ir reader");
 
-    llvm::LLVMContext Context;
-    llvm::SMDiagnostic Err;
-    auto module = llvm::parseIRFile(InputFile, Err, Context);
-    module->dump();
-    return 0;
+  llvm::LLVMContext Context;
+  llvm::SMDiagnostic Err;
+  std::unique_ptr<llvm::Module> module =
+      llvm::parseIRFile(InputFile, Err, Context);
+  module->print(llvm::outs(), nullptr);
+  return 0;
 }
