dc717cf505045a174fd8f919028f12dddba40a5b
yinchaogao <yinchaogao@deeproute.ai>
2023-11-27 17:46:20 +0800

    update printstack-when-exception

--- cpp/printstack-when-exception/preload.cpp
+++ cpp/printstack-when-exception/preload.cpp
@@ -7,8 +7,7 @@
 
 #include "stacktrace.h"
 
-extern "C" void *
-__cxxabiv1::__cxa_allocate_exception(std::size_t thrown_size) noexcept {
+extern "C" void *__cxa_allocate_exception(std::size_t thrown_size) noexcept {
   static void *(*func)(std::size_t) =
       (void *(*)(std::size_t))dlsym(RTLD_NEXT, "__cxa_allocate_exception");
   print_stack();
