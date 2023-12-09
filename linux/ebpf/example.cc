2a9611ac2108210b51e8401af70176a16c2dfc10
xundaoxd <xundaoxd@outlook.com>
2023-12-09 21:58:32 +0800

    add ebpf

---
+++ linux/ebpf/example.cc
@@ -0,0 +1,42 @@
+#include <bpf/libbpf.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/resource.h>
+#include <unistd.h>
+
+#include "example.skel.h"
+
+static volatile sig_atomic_t stop;
+static void sig_int(int signo) { stop = 1; }
+
+int main(int argc, char **argv) {
+  if (signal(SIGINT, sig_int) == SIG_ERR) {
+    fprintf(stderr, "can't set signal handler: %s\n", strerror(errno));
+    return 1;
+  }
+
+  struct example_bpf *skel;
+  skel = example_bpf::open_and_load();
+  if (!skel) {
+    fprintf(stderr, "Failed to open/load BPF skeleton\n");
+    return 1;
+  }
+
+  if (example_bpf::attach(skel)) {
+    fprintf(stderr, "Failed to attach BPF skeleton\n");
+    goto cleanup;
+  }
+
+  printf("Successfully started! Please run `sudo cat "
+         "/sys/kernel/debug/tracing/trace_pipe` "
+         "to see output of the BPF programs.\n");
+  while (!stop) {
+    sleep(1);
+  }
+
+  example_bpf::detach(skel);
+cleanup:
+  example_bpf::destroy(skel);
+  return 0;
+}
