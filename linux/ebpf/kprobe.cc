93d9ff0add294208c21811e6fac96b3828cb82f2
yinchaogao <yinchaogao@deeproute.ai>
2023-12-13 14:22:28 +0800

    add kprobe

--- linux/ebpf/kprobe.cc
+++ linux/ebpf/kprobe.cc
@@ -2,10 +2,11 @@
 #include <errno.h>
 #include <signal.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/resource.h>
 #include <unistd.h>
 
-#include "example_bpf.skel.h"
+#include "kprobe_bpf.skel.h"
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format,
                            va_list args) {
@@ -13,37 +14,45 @@ static int libbpf_print_fn(enum libbpf_print_level level, const char *format,
 }
 
 static volatile sig_atomic_t stop;
+
 static void sig_int(int signo) { stop = 1; }
 
 int main(int argc, char **argv) {
-  if (signal(SIGINT, sig_int) == SIG_ERR) {
-    fprintf(stderr, "can't set signal handler: %s\n", strerror(errno));
-    return 1;
-  }
+  struct kprobe_bpf *skel;
+  int err;
 
+  /* Set up libbpf errors and debug info callback */
   libbpf_set_print(libbpf_print_fn);
 
-  struct example_bpf *skel;
-  skel = example_bpf__open_and_load();
+  /* Open load and verify BPF application */
+  skel = kprobe_bpf__open_and_load();
   if (!skel) {
-    fprintf(stderr, "Failed to open/load BPF skeleton\n");
+    fprintf(stderr, "Failed to open BPF skeleton\n");
     return 1;
   }
 
-  if (example_bpf__attach(skel)) {
+  /* Attach tracepoint handler */
+  err = kprobe_bpf__attach(skel);
+  if (err) {
     fprintf(stderr, "Failed to attach BPF skeleton\n");
+    goto cleanup;
+  }
+
+  if (signal(SIGINT, sig_int) == SIG_ERR) {
+    fprintf(stderr, "can't set signal handler: %s\n", strerror(errno));
     goto cleanup;
   }
 
   printf("Successfully started! Please run `sudo cat "
          "/sys/kernel/debug/tracing/trace_pipe` "
          "to see output of the BPF programs.\n");
+
   while (!stop) {
+    fprintf(stderr, ".");
     sleep(1);
   }
 
-  example_bpf__detach(skel);
 cleanup:
-  example_bpf__destroy(skel);
-  return 0;
+  kprobe_bpf__destroy(skel);
+  return -err;
 }
