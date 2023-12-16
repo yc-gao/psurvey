57c3089319db17538ce65d04b4f5da57419fdbf1
xundaoxd <xundaoxd@outlook.com>
2023-12-16 20:51:52 +0800

    update promise

--- cpp/promise/demo.cc
+++ cpp/promise/demo.cc
@@ -3,10 +3,19 @@
 
 #include "Promise.h"
 
+struct Num {
+  int val{0};
+  Num(int val) : val(val) { std::cout << "Num(val)" << std::endl; }
+  Num() { std::cout << "Num()" << std::endl; }
+  Num(const Num &num) : val(num.val) { std::cout << "Num(&)" << std::endl; }
+  Num(Num &&num) : val(num.val) { std::cout << "Num(&&)" << std::endl; }
+};
+
 int main(int argc, char *argv[]) {
   Promise<int> num;
-  num.Then([](int num) { return num * num; })
-      .Then([](int num) { return std::to_string(num); })
+  num.Then([](int num) { return Num(num); })
+      .Then([](Num num) { return num.val * num.val; })
+      .Then([](Num num) { return std::to_string(num.val); })
       .Then([](std::string num) { std::cout << "num: " << num << std::endl; })
       .Catch([](const std::error_code &ec) {
         std::cerr << "error msg: " << ec.message() << std::endl;
@@ -19,4 +28,3 @@ int main(int argc, char *argv[]) {
   num.Reject(std::error_code());
   return 0;
 }
-
