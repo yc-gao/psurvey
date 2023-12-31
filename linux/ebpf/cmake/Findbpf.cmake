2d36f668ff8a493370e9bba88f86fc4efadde9f8
xundaoxd <xundaoxd@outlook.com>
2023-12-31 22:46:00 +0800

    update bpf

--- linux/ebpf/cmake/Findbpf.cmake
+++ linux/ebpf/cmake/Findbpf.cmake
@@ -1,5 +1,44 @@
+include(ExternalProject)
+ExternalProject_Add(libbpf-proj
+    PREFIX libbpf
+    GIT_REPOSITORY  https://github.com/libbpf/libbpf.git
+    GIT_TAG         20c0a9e3d7e7d4aeb283eae982543c9cacc29477
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND make
+        -C src
+        BUILD_STATIC_ONLY=1
+        DESTDIR=${CMAKE_CURRENT_BINARY_DIR}/libbpf
+        INCLUDEDIR=
+        LIBDIR=
+        UAPIDIR=
+        install install_uapi_headers
+    BUILD_IN_SOURCE TRUE
+    INSTALL_COMMAND ""
+    STEP_TARGETS build
+)
+set(libbpf_INCLUDE_DIRS "${CMAKE_CURRENT_BINARY_DIR}/libbpf")
+add_library(bpf STATIC IMPORTED)
+set_target_properties(bpf PROPERTIES
+    IMPORTED_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/libbpf/libbpf.a"
+    INTERFACE_INCLUDE_DIRECTORIES   "${libbpf_INCLUDE_DIRS}")
+
+ExternalProject_Add(bpftool-proj
+    PREFIX bpftool
+    GIT_REPOSITORY  https://github.com/libbpf/bpftool.git
+    GIT_TAG         v7.3.0
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND make bootstrap
+        -C src
+        OUTPUT=${CMAKE_CURRENT_BINARY_DIR}/bpftool
+    BUILD_IN_SOURCE TRUE
+    INSTALL_COMMAND ""
+    STEP_TARGETS build
+)
+add_executable(bpftool IMPORTED)
+set_target_properties(bpftool PROPERTIES
+    IMPORTED_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/bpftoolbootstrap/bpftool")
+
 find_program(CLANG NAMES clang-17 clang-16 clang-15 clang REQUIRED)
-find_program(BPFTOOL NAMES bpftool REQUIRED)
 
 macro(bpf_object name)
     if(${CMAKE_SYSTEM_PROCESSOR} MATCHES "x86_64")
@@ -23,12 +62,12 @@ macro(bpf_object name)
 
     foreach(src obj IN ZIP_LISTS srcs objs)
         add_custom_command(OUTPUT ${obj}
-            COMMAND ${CLANG} -target bpf -o ${obj} -c -D__TARGET_ARCH_${ARCH} -g -O2 ${CMAKE_CURRENT_SOURCE_DIR}/${src}
+            COMMAND ${CLANG} -target bpf -o ${obj} -c -D__TARGET_ARCH_${ARCH} -g -O2 -I${libbpf_INCLUDE_DIRS} -I/usr/include/${CMAKE_SYSTEM_PROCESSOR}-linux-gnu ${CMAKE_CURRENT_SOURCE_DIR}/${src}
             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
     endforeach()
     add_custom_command(OUTPUT ${name}.skel.h
-        COMMAND ${BPFTOOL} gen object ${name}.o ${objs}
-        COMMAND ${BPFTOOL} gen skeleton ${name}.o > ${name}.skel.h
+        COMMAND bpftool gen object ${name}.o ${objs}
+        COMMAND bpftool gen skeleton ${name}.o > ${name}.skel.h
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
         DEPENDS ${objs}
         VERBATIM)
