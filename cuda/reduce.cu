943fcbb97c044ae413f52ef81151dc8df7517908
xundaoxd <xundaoxd@outlook.com>
2024-01-21 01:01:39 +0800

    add cuda reduce

---
+++ cuda/reduce.cu
@@ -0,0 +1,27 @@
+#include <cstddef>
+#include <iterator>
+
+template <unsigned int N> struct Int2Mask;
+
+template <unsigned int N> struct Int2Mask {
+  constexpr static int value = (Int2Mask<N - 1>::value << 1) | 1;
+};
+
+template <> struct Int2Mask<0> {
+  constexpr static int value = 0;
+};
+
+template <typename T, unsigned int warpSize = 32>
+__device__ T warpReduce(T val) {
+  int laneId = threadIdx.x & 0x1f;
+  for (int i = 16; i >= 1; i /= 2)
+    val += __shfl_xor_sync(Int2Mask<warpSize>::value, val, i, warpSize);
+  return val;
+}
+
+template <typename It, typename T>
+__global__ void DeviceReduce(It beg, It end, It out) {
+  // TODO: impl
+}
+
+int main(int argc, char *argv[]) { return 0; }
