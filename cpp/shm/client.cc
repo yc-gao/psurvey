30c9ae9bfa4e6ee3b63cc4d8704a1ee6adaa9f7e
xundaoxd <xundaoxd@outlook.com>
2024-01-24 10:44:39 +0800

    update cpp shm

--- cpp/shm/client.cc
+++ cpp/shm/client.cc
@@ -1,3 +1,5 @@
+#include <fcntl.h>
+#include <sys/mman.h>
 #include <sys/shm.h>
 
 #include <iostream>
@@ -7,7 +9,8 @@
 
 const char *shm_path = "/sample";
 
-int main(int argc, char *argv[]) {
+int do_systemv() {
+
   key_t shm_key = ftok(shm_path, 0);
   int shmid = shmget(shm_key, sizeof(int), 0666);
   if (shmid == -1) {
@@ -28,3 +31,26 @@ int main(int argc, char *argv[]) {
   (*(int *)(shm_addr))++;
   return 0;
 }
+
+int do_posix() {
+  int fd = shm_open(shm_path, O_RDWR, 0777);
+  if (fd < 0) {
+    return 1;
+  }
+
+  void *buf = mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (buf == MAP_FAILED) {
+    return 1;
+  }
+  MAKE_DEFER(munmap(buf, 4096));
+
+  while (*(int *)(buf) != 100) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+  std::cout << "recv signal from server" << std::endl;
+  (*(int *)(buf))++;
+
+  return 0;
+}
+
+int main(int argc, char *argv[]) { return do_posix(); }
--- cpp/shm/server.cc
+++ cpp/shm/server.cc
@@ -1,4 +1,8 @@
+#include <fcntl.h>
+#include <sys/mman.h>
 #include <sys/shm.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include <iostream>
 #include <thread>
@@ -7,7 +11,7 @@
 
 const char *shm_path = "/sample";
 
-int main(int argc, char *argv[]) {
+int do_systemv() {
   key_t shm_key = ftok(shm_path, 0);
   int shmid = shmget(shm_key, sizeof(int), 0666 | IPC_CREAT);
   if (shmid == -1) {
@@ -28,3 +32,30 @@ int main(int argc, char *argv[]) {
   std::cout << "recv signal from client" << std::endl;
   return 0;
 }
+
+int do_posix() {
+  int fd = shm_open(shm_path, O_CREAT | O_RDWR, 0777);
+  if (fd < 0) {
+    return 1;
+  }
+  MAKE_DEFER(shm_unlink(shm_path));
+
+  if (ftruncate(fd, 4096)) {
+    return 1;
+  }
+
+  void *buf = mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (buf == MAP_FAILED) {
+    return 1;
+  }
+  MAKE_DEFER(munmap(buf, 4096));
+
+  *(int *)(buf) = 100;
+  while (*(int *)(buf) != 101) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+
+  return 0;
+}
+
+int main(int argc, char *argv[]) { return do_posix(); }
