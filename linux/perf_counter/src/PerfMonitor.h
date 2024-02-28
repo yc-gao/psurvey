257353570881879dec80975f5ddc2d3ee34fcccb
yinchaogao <yinchaogao@deeproute.ai>
2024-02-28 16:16:30 +0800

    save for debug

---
+++ linux/perf_counter/src/PerfMonitor.h
@@ -0,0 +1,108 @@
+#pragma once
+
+#include <cstdint>
+#include <cstring>
+
+#include <linux/perf_event.h>
+#include <memory>
+#include <stdexcept>
+#include <sys/ioctl.h>
+#include <sys/syscall.h>
+#include <system_error>
+#include <unistd.h>
+#include <unordered_map>
+
+class PerfMonitor {
+  int perf_fd;
+
+  std::uint64_t buf_size;
+  std::unique_ptr<char[]> buf;
+  std::unordered_map<std::uint64_t, std::uint64_t *> id2val;
+
+  std::uint64_t time_enabled;
+  std::uint64_t time_running;
+
+public:
+  PerfMonitor(std::size_t buf_size = 4096)
+      : perf_fd(-1), buf_size(buf_size), buf(new char[buf_size]) {}
+
+  void Monitor(struct perf_event_attr *attr, int pid = 0, int cpu = -1,
+               std::uint64_t *val = nullptr) {
+    int fd = syscall(SYS_perf_event_open, attr, pid, cpu, perf_fd, 0);
+    if (fd == -1) {
+      throw std::system_error(errno, std::generic_category());
+    }
+    if (perf_fd == -1) {
+      perf_fd = fd;
+    }
+    std::uint64_t perf_id;
+    if (-1 == ioctl(fd, PERF_EVENT_IOC_ID, &perf_id)) {
+      close(fd);
+      throw std::system_error(errno, std::generic_category());
+    }
+    id2val.emplace(perf_id, val);
+  }
+
+  void Monitor(perf_type_id type, std::uint64_t eventid, int pid = 0,
+               int cpu = -1, std::uint64_t *val = nullptr) {
+    struct perf_event_attr attr;
+    memset(&attr, 0, sizeof(attr));
+    attr.type = type;
+    attr.size = sizeof(attr);
+    attr.config = eventid;
+
+    attr.disabled = 1;
+    attr.exclude_kernel = 1;
+    attr.exclude_hv = 1;
+
+    attr.read_format = PERF_FORMAT_GROUP | PERF_FORMAT_ID |
+                       PERF_FORMAT_TOTAL_TIME_ENABLED |
+                       PERF_FORMAT_TOTAL_TIME_RUNNING;
+    Monitor(&attr, pid, cpu, val);
+  }
+  void Monitor(perf_hw_id eventid, int pid = 0, int cpu = -1,
+               std::uint64_t *val = nullptr) {
+    Monitor(PERF_TYPE_HARDWARE, pid, cpu, eventid, val);
+  }
+  void Monitor(perf_sw_ids eventid, int pid = 0, int cpu = -1,
+               std::uint64_t *val = nullptr) {
+    Monitor(PERF_TYPE_SOFTWARE, pid, cpu, eventid, val);
+  }
+
+  void Begin() {
+    if (-1 == ioctl(perf_fd, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP)) {
+      throw std::runtime_error("can not reset perf event");
+    }
+    if (-1 == ioctl(perf_fd, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP)) {
+      throw std::runtime_error("can not enable perf event");
+    }
+  }
+  void End() {
+    if (-1 == ioctl(perf_fd, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP)) {
+      throw std::runtime_error("can not enable perf event");
+    }
+    auto size = read(perf_fd, buf.get(), buf_size);
+    if (size == -1) {
+      throw std::system_error(errno, std::generic_category());
+    } else if (size == 0) {
+      throw std::runtime_error("can not read perf event");
+    }
+    struct read_format {
+      std::uint64_t nr;
+      std::uint64_t time_enabled;
+      std::uint64_t time_running;
+    };
+    struct read_item {
+      std::uint64_t value;
+      std::uint64_t id;
+    };
+    read_format *header = (read_format *)buf.get();
+    time_enabled = header->time_enabled;
+    time_running = header->time_running;
+
+    read_item *items = (read_item *)((char *)buf.get() + sizeof(read_format));
+    for (std::uint64_t i = 0; i < header->nr; i++) {
+      *id2val.at(items[i].id) = items[i].value;
+    }
+  }
+};
--- linux/perf_counter/src/main.cc
+++ linux/perf_counter/src/main.cc
@@ -1,160 +1,36 @@
-#include <csignal>
-#include <cstdio>
-#include <cstring>
+#include <cstdint>
 #include <iostream>
-
-#include <linux/hw_breakpoint.h> /* Definition of HW_* constants */
-#include <linux/perf_event.h>    /* Definition of PERF_* constants */
-#include <poll.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <sys/syscall.h> /* Definition of SYS_* constants */
-#include <system_error>
 #include <thread>
-#include <unistd.h>
 
-static long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
-                            int cpu, int group_fd, unsigned long flags) {
-  int ret;
-  ret = syscall(SYS_perf_event_open, hw_event, pid, cpu, group_fd, flags);
-  return ret;
+#include "PerfMonitor.h"
+
+int fib(int n) {
+  if (n == 1 || n == 0) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
 }
+void do_perf0() {
+  PerfMonitor monitor;
 
-bool running{true};
+  std::uint64_t cycles;
+  monitor.Monitor(PERF_COUNT_HW_CPU_CYCLES, 0, -1, &cycles);
 
-void do_payload() {
-  while (running) {
+  std::uint64_t insts;
+  monitor.Monitor(PERF_COUNT_HW_INSTRUCTIONS, 0, -1, &insts);
+
+  monitor.Begin();
+  for (int i = 0; i < 10; i++) {
+    std::cout << "fub10 " << fib(10) << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
-}
+  monitor.End();
 
-void do_read(pid_t pid = 266267) {
-  struct perf_event_attr attr;
-  std::memset(&attr, 0, sizeof(attr));
-  attr.size = sizeof(attr);
-
-  attr.type = PERF_TYPE_HARDWARE;
-  attr.config = PERF_COUNT_HW_INSTRUCTIONS;
-
-  attr.disabled = 1;
-  attr.exclude_kernel = 1;
-  attr.exclude_hv = 1;
-
-  attr.read_format = PERF_FORMAT_TOTAL_TIME_ENABLED |
-                     PERF_FORMAT_TOTAL_TIME_RUNNING | PERF_FORMAT_ID |
-                     PERF_FORMAT_LOST;
-
-  int perf_fd = perf_event_open(&attr, pid, -1, -1, PERF_FLAG_FD_CLOEXEC);
-  if (perf_fd == -1) {
-    throw std::system_error(errno, std::generic_category());
-  }
-  ioctl(perf_fd, PERF_EVENT_IOC_RESET, 0);
-  while (running) {
-    ioctl(perf_fd, PERF_EVENT_IOC_ENABLE, 0);
-    ioctl(perf_fd, PERF_EVENT_IOC_DISABLE, 0);
-    struct read_format {
-      std::uint64_t value;        /* The value of the event */
-      std::uint64_t time_enabled; /* if PERF_FORMAT_TOTAL_TIME_ENABLED */
-      std::uint64_t time_running; /* if PERF_FORMAT_TOTAL_TIME_RUNNING */
-      std::uint64_t id;           /* if PERF_FORMAT_ID */
-      std::uint64_t lost;         /* if PERF_FORMAT_LOST */
-    } buf;
-    auto l = read(perf_fd, &buf, sizeof(buf));
-    if (l < 0) {
-      throw std::system_error(errno, std::generic_category());
-    } else if (l == 0) {
-      continue;
-    } else {
-      std::cout << "time_enabled " << buf.time_enabled << " time_running "
-                << buf.time_running << " id " << buf.id << " lost " << buf.lost
-                << " value " << buf.value << std::endl;
-    }
-  }
-  close(perf_fd);
-}
-
-void do_period(pid_t pid = 291080) {
-  struct perf_event_attr attr;
-  std::memset(&attr, 0, sizeof(attr));
-  attr.size = sizeof(attr);
-
-  attr.type = PERF_TYPE_SOFTWARE;
-  attr.config = PERF_COUNT_SW_CPU_CLOCK;
-
-  // attr.freq = 1;
-  // attr.sample_freq = 10;
-  attr.sample_period = 100000;
-  attr.wakeup_events = 1;
-  attr.sample_type =
-      PERF_SAMPLE_TIME | PERF_SAMPLE_TID | PERF_SAMPLE_READ | PERF_SAMPLE_CPU;
-
-  attr.disabled = 1;
-  attr.exclude_kernel = 1;
-  attr.exclude_hv = 1;
-
-  int perf_fd = perf_event_open(&attr, pid, -1, -1, PERF_FLAG_FD_CLOEXEC);
-  if (perf_fd == -1) {
-    throw std::system_error(errno, std::generic_category());
-  }
-
-  int page_size = getpagesize();
-  int page_count = 16;
-  void *perf_mmap = mmap(0, page_size + page_count * page_size,
-                         PROT_READ | PROT_WRITE, MAP_SHARED, perf_fd, 0);
-  if (perf_mmap == MAP_FAILED) {
-    throw std::system_error(errno, std::generic_category());
-  }
-
-  ioctl(perf_fd, PERF_EVENT_IOC_RESET, 0);
-  ioctl(perf_fd, PERF_EVENT_IOC_ENABLE, 0);
-  struct pollfd pfd = {.fd = perf_fd, .events = POLLIN};
-  struct perf_event_mmap_page *perf_info =
-      (struct perf_event_mmap_page *)perf_mmap;
-  while (running) {
-    if (poll(&pfd, 1, -1) == -1) {
-      throw std::system_error(errno, std::generic_category());
-    }
-    struct perf_format {
-      std::uint32_t pid, tid; /* if PERF_SAMPLE_TID */
-      std::uint64_t time;     /* if PERF_SAMPLE_TIME */
-      // std::uint64_t addr;      /* if PERF_SAMPLE_ADDR */
-      // std::uint64_t id;        /* if PERF_SAMPLE_ID */
-      // std::uint64_t stream_id; /* if PERF_SAMPLE_STREAM_ID */
-      std::uint32_t cpu, res; /* if PERF_SAMPLE_CPU */
-      // std::uint64_t period;    /* if PERF_SAMPLE_PERIOD */
-      struct {
-        std::uint64_t value;
-      } v; /* if PERF_SAMPLE_READ */
-    };
-
-    while (perf_info->data_tail < perf_info->data_head) {
-      struct perf_event_header *header =
-          (struct perf_event_header *)((char *)perf_mmap +
-                                       perf_info->data_offset +
-                                       (perf_info->data_tail % page_size));
-
-      if (header->type == PERF_RECORD_SAMPLE) {
-        perf_format *perf_data =
-            (perf_format *)((char *)header + sizeof(perf_event_header));
-        std::cout << header->size << "\t" << perf_data->time << "\t"
-                  << perf_data->v.value << "\n";
-        break;
-      }
-      perf_info->data_tail += header->size;
-    }
-  }
-  ioctl(perf_fd, PERF_EVENT_IOC_DISABLE, 0);
-
-  munmap(perf_mmap, page_count * page_size);
-  close(perf_fd);
+  std::cout << "cycles " << cycles << std::endl;
+  std::cout << "insts " << insts << std::endl;
 }
 
 int main(int argc, char *argv[]) {
-  std::signal(SIGINT, [](int sig) { running = false; });
-
-  std::thread t(do_payload);
-
-  // do_read();
-  do_period();
-
+  do_perf0();
   return 0;
 }
