faf0ab670ad59f3e571e78482f0a50b0d0dfe793
xundaoxd <xundaoxd@outlook.com>
2024-06-18 11:35:57 +0800

    update cuda

--- benchmarks/cuda/src/copy_throughput.cu
+++ benchmarks/cuda/src/copy_throughput.cu
@@ -1,4 +1,6 @@
 #include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
+#include <thrust/functional.h>
 
 #include <nvbench/nvbench.cuh>
 
@@ -21,29 +23,34 @@ void cuda_copy(nvbench::state &state) {
 }
 NVBENCH_BENCH(cuda_copy);
 
-void thrust_copy(nvbench::state &state) {
-  const std::size_t num_values = 64 * 1024 * 1024 / sizeof(nvbench::int32_t);
-  thrust::device_vector<nvbench::int32_t> input(num_values);
-  thrust::device_vector<nvbench::int32_t> output(num_values);
+template <typename T>
+void thrust_copy(nvbench::state &state, nvbench::type_list<T>) {
+  const std::size_t num_values = 64 * 1024 * 1024 / sizeof(T);
+  thrust::device_vector<T> input(num_values);
+  thrust::device_vector<T> output(num_values);
 
   // Provide throughput information:
   state.add_element_count(num_values, "NumElements");
-  state.add_global_memory_reads<nvbench::int32_t>(num_values);
-  state.add_global_memory_writes<nvbench::int32_t>(num_values);
+  state.add_global_memory_reads<T>(num_values);
+  state.add_global_memory_writes<T>(num_values);
 
   state.exec(nvbench::exec_tag::sync,
              [=, &input, &output](nvbench::launch &launch) {
-               thrust::copy(output.begin(), output.end(), input.begin());
+               thrust::transform(thrust::device, input.begin(), input.end(),
+                                 output.begin(), thrust::negate<T>());
              });
 }
-NVBENCH_BENCH(thrust_copy);
+NVBENCH_BENCH_TYPES(
+    thrust_copy,
+    NVBENCH_TYPE_AXES(nvbench::type_list<nvbench::int8_t, nvbench::int16_t,
+                                         nvbench::int32_t, nvbench::int64_t>));
 
 template <typename T>
 __global__ void kernel_copy_impl(T *dest, const T *src, int N) {
   auto step = gridDim.x * blockDim.x;
   auto tid = blockIdx.x * blockDim.x + threadIdx.x;
   for (; tid < N; tid += step) {
-    dest[tid] = src[tid];
+    dest[tid] = -src[tid];
   }
 }
 template <typename T>
