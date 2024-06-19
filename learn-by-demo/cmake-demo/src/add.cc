6a31e023b73878119b37d428042ca58561665ab4
xundaoxd <xundaoxd@outlook.com>
2024-06-19 11:26:52 +0800

    add cmake-demo

---
+++ learn-by-demo/cmake-demo/src/add.cc
@@ -0,0 +1,3 @@
+#include "demo/add.h"
+
+int add(int a, int b) { return a + b; }
---
+++ learn-by-demo/cmake-demo/src/hello.cc
@@ -0,0 +1,5 @@
+#include <iostream>
+int main(int, char *[]) {
+  std::cout << "hello" << std::endl;
+  return 0;
+}
