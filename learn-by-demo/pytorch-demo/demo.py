2b1fc88717e5f8becf79a4820d88446c9046880b
xundaoxd <xundaoxd@outlook.com>
2024-06-19 17:06:24 +0800

    update

--- learn-by-demo/pytorch-demo/demo.py
+++ learn-by-demo/pytorch-demo/demo.py
@@ -6,6 +6,9 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+from model import NeuralNetwork
+
+
 device = (
     "cuda"
     if torch.cuda.is_available()
@@ -28,24 +31,6 @@ classes = [
 ]
 
 batch_size = 64
-
-
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10)
-        )
-
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
 
 
 def make_dataset():
