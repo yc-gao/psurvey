6a585f0abc1724de66c78b64af5cfeb6d70117e2
xundaoxd <xundaoxd@outlook.com>
2024-06-24 09:52:13 +0800

    update ebpf-demo

--- learn-by-demo/ebpf-demo/cmake/Findblazesym.cmake
+++ learn-by-demo/ebpf-demo/cmake/Findblazesym.cmake
@@ -1,22 +1,20 @@
-include(ExternalProject)
+if(blazesym_ADDED)
+    return()
+endif()
 
-ExternalProject_Add(blazesym-proj
-    GIT_REPOSITORY      https://github.com/libbpf/blazesym.git
-    GIT_TAG             v0.2.0-rc.0
-    CONFIGURE_COMMAND   ""
-    BUILD_IN_SOURCE     True
-    BUILD_COMMAND       cargo build --package=blazesym-c --release
-    INSTALL_COMMAND     ""
+CPMAddPackage("gh:libbpf/blazesym#v0.2.0-rc.0")
+execute_process(
+    WORKING_DIRECTORY ${blazesym_SOURCE_DIR}
+    COMMAND cargo build --package=blazesym-c --release
 )
-ExternalProject_Get_Property(blazesym-proj SOURCE_DIR)
 
-set(blazesym_prefix ${SOURCE_DIR})
-set(blazesym_INCLUDE_DIRS "${blazesym_prefix}/capi/include")
-set(blazesym_LIBRARIES "${blazesym_prefix}/target/release/libblazesym_c.a")
+find_path(blazesym_INCLUDE_DIR NAMES blazesym.h PATHS ${blazesym_SOURCE_DIR}/capi/include REQUIRED)
+find_library(blazesym_LIBRARY NAMES blazesym_c PATHS ${blazesym_SOURCE_DIR}/target/release REQUIRED)
+
 add_library(blazesym::blazesym STATIC IMPORTED)
 set_target_properties(blazesym::blazesym PROPERTIES
-    IMPORTED_LOCATION "${blazesym_LIBRARIES}"
-    INTERFACE_INCLUDE_DIRECTORIES "${blazesym_INCLUDE_DIRS}"
+    IMPORTED_LOCATION "${blazesym_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${blazesym_INCLUDE_DIR}"
 )
 target_link_libraries(blazesym::blazesym INTERFACE pthread rt dl m)
 add_dependencies(blazesym::blazesym blazesym-proj)
