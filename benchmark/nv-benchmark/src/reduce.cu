069eaae9c03bb32edf7fd939b7e7c6e95792da78
yinchaogao <yinchaogao@deeproute.ai>
2024-07-17 14:22:44 +0800

    update

--- benchmark/nv-benchmark/src/reduce.cu
+++ benchmark/nv-benchmark/src/reduce.cu
@@ -47,6 +47,7 @@ void cub_reduce(nvbench::state &state, nvbench::type_list<T>) {
                             input.size(), thrust::plus<T>(), T());
   thrust::device_vector<char> d_temp(temp_storage_bytes);
   d_temp_storage = thrust::raw_pointer_cast(d_temp.data());
+  std::cout << "temp bytes: " << temp_storage_bytes << std::endl;
 
   // Provide throughput information:
   state.add_element_count(num_values, "NumElements");
@@ -68,6 +69,52 @@ void cub_reduce(nvbench::state &state, nvbench::type_list<T>) {
 }
 NVBENCH_BENCH_TYPES(
     cub_reduce,
+    NVBENCH_TYPE_AXES(
+        nvbench::type_list<nvbench::int8_t, nvbench::int16_t, nvbench::int32_t,
+                           nvbench::int64_t, nvbench::float32_t,
+                           nvbench::float64_t>));
+
+template <typename T>
+__global__ void kernel_reduce_impl(const T *input, std::size_t size,
+                                   T *output) {
+  auto sum = T();
+  for (auto idx = blockIdx.x * blockDim.x + threadIdx.x; idx < size;
+       idx += gridDim.x * blockDim.x) {
+    sum += input[idx];
+  }
+  output[blockIdx.x] = sum;
+}
+
+template <typename T>
+void kernel_reduce(nvbench::state &state, nvbench::type_list<T>) {
+  // 64MB
+  const std::size_t num_values = 64 * 1024 * 1024 / sizeof(T);
+  thrust::device_vector<T> input(num_values);
+  thrust::device_vector<T> output(1);
+  thrust::device_vector<T> temp(256);
+
+  // Provide throughput information:
+  state.add_element_count(num_values, "NumElements");
+  state.add_global_memory_reads<T>(num_values);
+
+  state.collect_dram_throughput();
+  state.collect_l1_hit_rates();
+  state.collect_l2_hit_rates();
+  state.collect_loads_efficiency();
+  state.collect_stores_efficiency();
+
+  state.exec(nvbench::exec_tag::sync, [=, &input, &output,
+                                       &temp](nvbench::launch &launch) mutable {
+    kernel_reduce_impl<T><<<256, 256>>>(thrust::raw_pointer_cast(input.data()),
+                                        input.size(),
+                                        thrust::raw_pointer_cast(temp.data()));
+    kernel_reduce_impl<T><<<1, 256>>>(thrust::raw_pointer_cast(temp.data()),
+                                      temp.size(),
+                                      thrust::raw_pointer_cast(output.data()));
+  });
+}
+NVBENCH_BENCH_TYPES(
+    kernel_reduce,
     NVBENCH_TYPE_AXES(
         nvbench::type_list<nvbench::int8_t, nvbench::int16_t, nvbench::int32_t,
                            nvbench::int64_t, nvbench::float32_t,
