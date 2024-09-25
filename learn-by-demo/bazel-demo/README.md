3cde381ccb9b42cfbacca349f512ebcfc422b8bb
ycgao <yc_x@outlook.com>
2024-09-25 23:36:21 +0800

    update

--- learn-by-demo/bazel-demo/README.md
+++
@@ -1,6 +0,0 @@
-# bazel demo
-
-```shell
-bazel run @hedron_compile_commands//:refresh_all
-bazel run :main
-```
--- learn-by-demo/bazel-demo/main.cc
+++
@@ -1,9 +0,0 @@
-#include <glog/logging.h>
-
-int main(int argc, char* argv[]) {
-  // Initialize Google’s logging library.
-  google::InitGoogleLogging(argv[0]);
-
-  int num_cookies = 42;
-  LOG(INFO) << "Found " << num_cookies << " cookies";
-}
