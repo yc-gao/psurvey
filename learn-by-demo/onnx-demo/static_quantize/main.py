d9679d3299931ab1805edd02395c02ae9c11d7de
ycgao <yc_x@outlook.com>
2024-11-22 05:55:11 +0800

    update

--- learn-by-demo/onnx-demo/static_quantize/main.py
+++ learn-by-demo/onnx-demo/static_quantize/main.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 import argparse
 import random
+import tempfile
+from pathlib import Path
 
 import numpy as np
 
-from onnxruntime.quantization import QuantFormat, QuantType, quantize_static
-from onnxruntime.quantization.calibrate import CalibrationDataReader, CalibrationMethod
+import onnx
+
+from onnxruntime.quantization.quant_utils import QuantType
+from onnxruntime.quantization.calibrate import CalibrationDataReader, CalibrationMethod, create_calibrator
+from onnxruntime.quantization.qdq_quantizer import QDQQuantizer
+from onnxruntime.quantization.registry import QLinearOpsRegistry, QDQRegistry
 
 
 class FakeResnetCalibrationDataReader(CalibrationDataReader):
@@ -18,7 +24,7 @@ class FakeResnetCalibrationDataReader(CalibrationDataReader):
 
     def get_next(self) -> dict:
         try:
-            return {"data": next(self.iterator)[0]}
+            return {"input.1": next(self.iterator)[0]}
         except Exception:
             return None
 
@@ -26,6 +32,7 @@ class FakeResnetCalibrationDataReader(CalibrationDataReader):
 # https://github.com/onnx/models/blob/main/validated/vision/classification/resnet/model/resnet50-v2-7.onnx
 def parse_options():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--cache')
     parser.add_argument('-o', '--output', type=str, default='output.onnx')
     parser.add_argument('model')
     return parser.parse_args()
@@ -33,12 +40,52 @@ def parse_options():
 
 def main():
     options = parse_options()
+    calibration_data_reader = FakeResnetCalibrationDataReader(16)
 
-    dataloader = FakeResnetCalibrationDataReader(16)
-    quantize_static(
-        options.model,
-        options.output,
-        calibration_data_reader=dataloader)
+    extra_options = {}
+    op_types_to_quantize = []
+    nodes_to_quantize = []
+    nodes_to_exclude = []
+
+    if not op_types_to_quantize:
+        q_linear_ops = list(QLinearOpsRegistry.keys())
+        qdq_ops = list(QDQRegistry.keys())
+        op_types_to_quantize = list(set(q_linear_ops + qdq_ops))
+
+    with tempfile.TemporaryDirectory() as tmp:
+        model_path = Path(options.model)
+        inferred_model_path = Path(
+            options.cache or tmp)/(model_path.stem + "-inferred" + model_path.suffix)
+        onnx.shape_inference.infer_shapes_path(
+            str(model_path), str(inferred_model_path))
+        model = onnx.load(inferred_model_path)
+
+        calibrator = create_calibrator(
+            inferred_model_path,
+            op_types_to_quantize,
+            augmented_model_path=Path(inferred_model_path).parent.joinpath(
+                "augmented_model.onnx").as_posix(),
+            calibrate_method=CalibrationMethod.MinMax,
+        )
+        calibrator.collect_data(calibration_data_reader)
+        tensors_range = calibrator.compute_data()
+        del calibrator
+
+        quantizer = QDQQuantizer(
+            model,
+            False,
+            False,
+            QuantType.QInt8,
+            QuantType.QInt8,
+            tensors_range,
+            nodes_to_quantize,
+            nodes_to_exclude,
+            op_types_to_quantize,
+            extra_options
+        )
+        quantizer.quantize_model()
+        if options.output:
+            quantizer.model.save_model_to_file(options.output)
 
 
 if __name__ == "__main__":
