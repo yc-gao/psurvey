b7a0d3877f13adda296228af5c107ca065b8fbc5
yinchaogao <yinchaogao@deeproute.ai>
2024-11-21 21:10:02 +0800

    update

--- learn-by-demo/onnx-demo/onnx_modify/main.py
+++ learn-by-demo/onnx-demo/onnx_modify/main.py
@@ -134,14 +134,14 @@ class OnnxModel:
 
     def remove_inputs_unused(self):
         tmp = set(functools.reduce(lambda a, b: a + b,
-                  [node.input for node in self.nodes()]))
+                  [list(node.input) for node in self.nodes()]))
         inputs_unused = filter(
             lambda x: x.name not in tmp, self.inputs())
         self.remove_inputs(inputs_unused)
 
     def remove_initializers_unused(self):
         tmp = set(functools.reduce(lambda a, b: a + b,
-                  [node.input for node in self.nodes()]))
+                  [list(node.input) for node in self.nodes()]))
         initializers_unused = filter(
             lambda x: x.name not in tmp, self.initializers())
         self.remove_initializers(initializers_unused)
