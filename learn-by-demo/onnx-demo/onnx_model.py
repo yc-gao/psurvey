92711b2326e00e414dded2e8c999a8948e8c0af2
yinchaogao <yinchaogao@deeproute.ai>
2024-11-26 13:47:58 +0800

    update

--- learn-by-demo/onnx-demo/onnx_model.py
+++ learn-by-demo/onnx-demo/onnx_model.py
@@ -1,4 +1,5 @@
-import functools
+#!/usr/bin/env python3
+import argparse
 
 import onnx
 
@@ -243,3 +244,23 @@ class OnnxModel:
             new_name = io_maps.get(vinfo.name, None)
             if new_name:
                 vinfo.name = new_name
+
+
+def parse_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output')
+    parser.add_argument('model')
+    return parser.parse_args()
+
+
+def main():
+    options = parse_options()
+    model = OnnxModel(options.model)
+
+    model.topological_sort()
+    if options.output:
+        onnx.save(model.model, options.output)
+
+
+if __name__ == "__main__":
+    main()
