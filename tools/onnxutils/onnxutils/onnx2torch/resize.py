20c5d031310e46155c308d20db967de0c29ba1dc
yinchaogao <yinchaogao@deeproute.ai>
2024-12-30 19:27:45 +0800

    update resize

--- tools/onnxutils/onnxutils/onnx2torch/resize.py
+++ tools/onnxutils/onnxutils/onnx2torch/resize.py
@@ -20,12 +20,11 @@ mode_mapping = {
 
 
 class TorchResize(nn.Module, OnnxToTorchModule):
-    def __init__(self, scales, sizes, mode, align_corners):
+    def __init__(self, scales, sizes, mode):
         super().__init__()
         self.scales = scales
         self.sizes = sizes
         self.mode = mode
-        self.align_corners = align_corners
 
     def forward(self, x):
         x = nn.functional.interpolate(
@@ -33,7 +32,6 @@ class TorchResize(nn.Module, OnnxToTorchModule):
             size=self.sizes,
             scale_factor=self.scales,
             mode=self.mode,
-            align_corners=self.align_corners,
         )
         return x
 
@@ -49,46 +47,17 @@ def _(onnx_node: OnnxNode, onnx_model: OnnxModel) -> OperationConverterResult:  
     mode = onnx_node.attributes().get('mode', 'nearest')
     nearest_mode = onnx_node.attributes().get('nearest_mode', 'round_prefer_floor')
 
-    if mode == 'nearest':
-        if nearest_mode != 'floor':
-            warnings.warn(
-                'Pytorch\'s nearest neighbor interpolate uses the "floor" nearest_mode. '
-                'For others modes, the results might differ significantly!'
-            )
+    assert onnx_node.inputs()[1] == '', "not implement"
 
-        if coordinate_transformation_mode != 'asymmetric':
-            warnings.warn(
-                'Pytorch\'s nearest neighbor interpolation uses "asymmetric" coordinate_transformation_mode. '
-                'For others modes, the results might differ significantly!'
-            )
-    else:
-        if coordinate_transformation_mode not in ['pytorch_half_pixel', 'half_pixel']:
-            warnings.warn(
-                'For linear and cubic interpolation in "asymmetric" and "align_corners" coordinate_transformation_mode'
-                'results might differ significantly!'
-            )
-
-    if cubic_coeff_a != -0.75:
-        warnings.warn(
-            'With a cubic coefficient value other than 0.75, the results might differ significantly!')
-
-    if exclude_outside != 0:
-        warnings.warn(
-            'With a exclude outside value other than 0, the results might differ significantly!')
-
-    if extrapolation_value != 0.0:
-        warnings.warn(
-            'With a extrapolation value other than 0.0, the results might differ significantly!')
-
-    roi, scales, sizes = ([
-        onnx_model.get_initializer_by_name(x)
-        for x in onnx_node.inputs()[1:]] + [None])[:3]
-    assert roi is None, "not implement"
+    scales = onnx_model.get_initializer_by_name(onnx_node.inputs()[2])
     if scales is not None:
         scales = scales.to_numpy().tolist()
         assert scales[:2] == [1, 1], "not implement"
         scales = scales[2:]
         torch_mode = mode_mapping[(mode, len(scales))]
+
+    sizes = onnx_model.get_initializer_by_name(
+        onnx_node.inputs()[3]) if len(onnx_node.inputs()) >= 4 else None
     if sizes is not None:
         sizes = sizes.to_numpy().tolist()
         vinfo = onnx_model.get_vinfo_by_name(onnx_node.inputs()[0])
@@ -98,12 +67,41 @@ def _(onnx_node: OnnxNode, onnx_model: OnnxModel) -> OperationConverterResult:  
         sizes = sizes[2:]
         torch_mode = mode_mapping[(mode, len(sizes))]
 
-    torch_align_corners = coordinate_transformation_mode == 'align_corners'
-    return OperationConverterResult(
-        torch_module=TorchResize(
-            scales, sizes, torch_mode, torch_align_corners),
-        onnx_mapping=OnnxMapping(
-            inputs=onnx_node.inputs()[:1],
-            outputs=onnx_node.outputs(),
-        ),
-    )
+    if coordinate_transformation_mode == 'asymmetric':
+        if cubic_coeff_a != -0.75:
+            warnings.warn('results might differ significantly!')
+        if exclude_outside != 0:
+            warnings.warn('results might differ significantly!')
+        if extrapolation_value != 0:
+            warnings.warn('results might differ significantly!')
+        if mode == 'nearest' and nearest_mode != 'floor':
+            warnings.warn('results might differ significantly!')
+        return OperationConverterResult(
+            torch_module=nn.Upsample(
+                size=tuple(sizes),
+                scale_factor=tuple(scales),
+                mode=torch_mode,
+            ),
+            onnx_mapping=OnnxMapping(
+                inputs=onnx_node.inputs()[:1],
+                outputs=onnx_node.outputs(),
+            ),
+        )
+    elif coordinate_transformation_mode == 'half_pixel':
+        if cubic_coeff_a != -0.75:
+            warnings.warn('results might differ significantly!')
+        if exclude_outside != 0:
+            warnings.warn('results might differ significantly!')
+        if extrapolation_value != 0:
+            warnings.warn('results might differ significantly!')
+        if mode == 'nearest' and nearest_mode != 'floor':
+            warnings.warn('results might differ significantly!')
+        return OperationConverterResult(
+            torch_module=TorchResize(scales, sizes, torch_mode),
+            onnx_mapping=OnnxMapping(
+                inputs=onnx_node.inputs()[:1],
+                outputs=onnx_node.outputs(),
+            ),
+        )
+    else:
+        raise NotImplementedError()
