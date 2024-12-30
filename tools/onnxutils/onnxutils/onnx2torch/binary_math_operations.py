1df6210f9d0723704e8a93009dcb439fa7d9d8ef
yinchaogao <yinchaogao@deeproute.ai>
2024-12-30 19:51:06 +0800

    support greater

--- tools/onnxutils/onnxutils/onnx2torch/binary_math_operations.py
+++ tools/onnxutils/onnxutils/onnx2torch/binary_math_operations.py
@@ -7,6 +7,11 @@ from onnxutils.common import OnnxModel, OnnxNode
 from .registry import converter
 from .utils import OnnxToTorchModule, OperationConverterResult, onnx_mapping_from_node
 
+func_mapping = {
+    'Add': torch.add,
+    'Greater': torch.gt,
+}
+
 
 class TorchBinaryOp(nn.Module, OnnxToTorchModule):
     def __init__(self, f):
@@ -17,9 +22,10 @@ class TorchBinaryOp(nn.Module, OnnxToTorchModule):
         return self.f(x0, x1)
 
 
+@converter(operation_type='Greater', version=13)
 @converter(operation_type='Add', version=14)
 def _(onnx_node: OnnxNode, onnx_model: OnnxModel) -> OperationConverterResult:
     return OperationConverterResult(
-        torch_module=TorchBinaryOp(torch.add),
+        torch_module=TorchBinaryOp(func_mapping[onnx_node.op_type()]),
         onnx_mapping=onnx_mapping_from_node(onnx_node),
     )
