c720ddd371482f0500ae2ffe9fda419cf60b18f8
yinchaogao <yinchaogao@deeproute.ai>
2025-01-03 17:32:25 +0800

    update

--- tools/onnxutils/onnxutils/common/dataset.py
+++ tools/onnxutils/onnxutils/common/dataset.py
@@ -15,6 +15,21 @@ class DatasetUtils:
         return InnerDataset(dataset, f)
 
     @staticmethod
+    def drop_front(dataset, n):
+        class InnerDataset:
+            def __init__(self, dataset, n):
+                self._dataset = dataset
+                self._n = n
+
+            def __len__(self):
+                return len(self._dataset) - self._n
+
+            def __getitem__(self, idx):
+                return self._dataset[idx + self._n]
+
+        return InnerDataset(dataset, min(n, len(dataset)))
+
+    @staticmethod
     def take_front(dataset, n):
         class InnerDataset:
             def __init__(self, dataset, n):
