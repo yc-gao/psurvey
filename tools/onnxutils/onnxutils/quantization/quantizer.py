32c53e3feb1c194efa087bd652fddeb0f46197f9
yinchaogao <yinchaogao@deeproute.ai>
2025-01-06 19:21:58 +0800

    update

--- tools/onnxutils/onnxutils/quantization/quantizer.py
+++ tools/onnxutils/onnxutils/quantization/quantizer.py
@@ -9,12 +9,61 @@ from .utils import partition_module_name, get_new_attr_name_with_prefix
 class BasicQuantizer:
     # [
     #     {
+    #         'name': '',
+    #         'module_type': '',
+    #         'activation': '',
+    #         'weight': ''
+    #     },
+    #     {
+    #         'op': '',
+    #         'module_type': '',
+    #         'activation': '',
+    #         'weight': ''
+    #     }
+    # ]
+    def quantize_nodes(self, graph_module: torch.fx.GraphModule, qconfigs):
+        node_name_to_qconfig = {
+            qconfig['name']: qconfig for qconfig in qconfigs if 'name' in qconfig
+        }
+        node_op_to_qconfig = {
+            qconfig['op']: qconfig for qconfig in qconfigs if 'op' in qconfig
+        }
+        for node in graph_module.graph.nodes:
+            qconfig = node_name_to_qconfig.get(node.name, None)
+            qconfig = qconfig or node_op_to_qconfig.get(node.op, None)
+            if qconfig is None:
+                continue
+
+            node.meta['qconfig'] = qconfig
+
+            act_qconfig = qconfig.get('activation', None)
+            if act_qconfig is None:
+                continue
+
+            fq_name = get_new_attr_name_with_prefix(graph_module, "fq")
+            graph_module.add_submodule(fq_name, act_qconfig())
+
+            with graph_module.graph.inserting_after(node):
+                fq_node = graph_module.graph.create_node(
+                    'call_module',
+                    fq_name
+                )
+                node.replace_all_uses_with(fq_node)
+                fq_node.args = (node,)
+        graph_module = torch.fx.GraphModule(graph_module, graph_module.graph)
+
+        graph_module = QatConverter.ConvertToQatModule(graph_module)
+        return graph_module
+
+    # [
+    #     {
     #         'module_name': '',
     #         'module_type': '',
     #         'activation': '',
     #         'weight': ''
     #     }
     # ]
+
     def quantize_modules(self, graph_module: torch.fx.GraphModule, qconfigs):
         module_name_to_qconfig = {
             qconfig['module_name']: qconfig for qconfig in qconfigs if 'module_name' in qconfig
