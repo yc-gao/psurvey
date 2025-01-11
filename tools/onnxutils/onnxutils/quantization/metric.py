aab42d67f9956018b5c04a1f0ed5330897efedb3
ycgao <yc_x@outlook.com>
2025-01-11 16:27:52 +0800

    update

--- tools/onnxutils/onnxutils/quantization/metric.py
+++ tools/onnxutils/onnxutils/quantization/metric.py
@@ -3,151 +3,114 @@ import copy
 import torch
 
 
+def do_compute(real, pred, fn):
+    assert type(real) is type(pred)
+
+    if isinstance(real, torch.Tensor):
+        return fn(real, pred)
+
+    if isinstance(real, (list, tuple)):
+        return [__compute(a, b, fn) for a, b in zip(real, pred)]
+
+    if isinstance(real, dict):
+        return {
+            __compute(real.get(key, None), pred.get(key, None), fn)
+            for key in real.keys() | pred.keys()
+        }
+
+    raise NotImplementedError
+
+
 def compute_mse(real, pred, reduction='none'):
-    assert real.shape == pred.shape
-    if real.ndim == 1:
-        real = real.unsqueeze(0)
-        pred = pred.unsqueeze(0)
+    def inner_kernel(real, pred):
+        assert real.shape == pred.shape
+        if real.dim() < 2:
+            real = real.reshape(-1, 1)
+            pred = pred.reshape(-1, 1)
 
-    real = real.flatten(start_dim=1).float()
-    pred = pred.flatten(start_dim=1).float()
+        real = real.flatten(start_dim=1).float()
+        pred = pred.flatten(start_dim=1).float()
 
-    mse = torch.pow(real - pred, 2).mean(dim=-1)
-    if reduction == 'mean':
-        return mse.mean()
-    if reduction == 'max':
-        return mse.max()
-    if reduction == 'min':
-        return mse.min()
-    elif reduction == 'sum':
-        return mse.sum()
-    elif reduction == 'none':
-        return mse
-    else:
+        metrics = torch.pow(real - pred, 2).mean(dim=-1)
+
+        if reduction == 'none':
+            return metrics
+        if reduction == 'max':
+            return metrics.max()
+        if reduction == 'min':
+            return metrics.min()
+        if reduction == 'mean':
+            return metrics.mean()
+        if reduction == 'sum':
+            return metrics.sum()
+
         raise NotImplementedError
+    return do_compute(real, pred, inner_kernel)
 
 
 def compute_cosine(real, pred, reduction='none'):
-    assert real.shape == pred.shape
-    if real.ndim == 1:
-        real = real.unsqueeze(0)
-        pred = pred.unsqueeze(0)
+    def inner_kernel(real, pred):
+        assert real.shape == pred.shape
+        if real.dim() < 2:
+            real = real.reshape(-1, 1)
+            pred = pred.reshape(-1, 1)
 
-    real = real.flatten(start_dim=1).float()
-    pred = pred.flatten(start_dim=1).float()
+        real = real.flatten(start_dim=1).float()
+        pred = pred.flatten(start_dim=1).float()
 
-    cosine_sim = torch.cosine_similarity(real, pred, dim=-1)
-    if reduction == 'mean':
-        return cosine_sim.mean()
-    if reduction == 'max':
-        return cosine_sim.max()
-    if reduction == 'min':
-        return cosine_sim.min()
-    elif reduction == 'sum':
-        return cosine_sim.sum()
-    elif reduction == 'none':
-        return cosine_sim
-    else:
+        metrics = torch.cosine_similarity(real, pred, dim=-1)
+
+        if reduction == 'none':
+            return metrics
+        if reduction == 'max':
+            return metrics.max()
+        if reduction == 'min':
+            return metrics.min()
+        if reduction == 'mean':
+            return metrics.mean()
+        if reduction == 'sum':
+            return metrics.sum()
+
         raise NotImplementedError
+    return do_compute(real, pred, inner_kernel)
 
 
 def compute_snr(real, pred, reduction='none'):
-    assert real.shape == pred.shape
-    if real.ndim == 1:
-        real = real.unsqueeze(0)
-        pred = pred.unsqueeze(0)
+    def inner_kernel(real, pred):
+        assert real.shape == pred.shape
+        if real.dim() < 2:
+            real = real.reshape(-1, 1)
+            pred = pred.reshape(-1, 1)
 
-    real = real.flatten(start_dim=1).float()
-    pred = pred.flatten(start_dim=1).float()
+        real = real.flatten(start_dim=1).float()
+        pred = pred.flatten(start_dim=1).float()
 
-    signal_power = torch.pow(real, 2).sum(dim=-1)
-    noise_power = torch.pow(real - pred, 2).sum(dim=-1)
+        metrics = (torch.pow(real - pred, 2) / torch.pow(real, 2)).sum(dim=-1)
 
-    snr = (noise_power) / (signal_power + 1e-7)
-    if reduction == 'mean':
-        return snr.mean()
-    if reduction == 'max':
-        return snr.max()
-    if reduction == 'min':
-        return snr.min()
-    elif reduction == 'sum':
-        return snr.sum()
-    elif reduction == 'none':
-        return snr
-    else:
+        if reduction == 'none':
+            return metrics
+        if reduction == 'max':
+            return metrics.max()
+        if reduction == 'min':
+            return metrics.min()
+        if reduction == 'mean':
+            return metrics.mean()
+        if reduction == 'sum':
+            return metrics.sum()
+
         raise NotImplementedError
+    return do_compute(real, pred, inner_kernel)
 
 
-def compute_max(t):
-    if t.ndim == 1:
-        t = t.unsqueeze(0)
-    t = t.flatten(start_dim=1).float()
-    return t.max(dim=1).values
-
-
-def compute_min(t):
-    if t.ndim == 1:
-        t = t.unsqueeze(0)
-    t = t.flatten(start_dim=1).float()
-    return t.min(dim=1).values
-
-
-def compute_mean(t):
-    if t.ndim == 1:
-        t = t.unsqueeze(0)
-    t = t.flatten(start_dim=1).float()
-    return t.mean(dim=1)
-
-
-def compute_range(t):
-    return compute_max(t) - compute_min(t)
-
-
-def compute_metrics(recorder0, recorder1, metrics, *args, **kwargs):
-    def compute(val0, val1, metric, *args, **kwargs):
-        if metric == 'mse':
-            val = compute_mse(val0, val1, *args, **kwargs)
-        elif metric == 'cosine':
-            val = compute_cosine(val0, val1, *args, **kwargs)
-        elif metric == 'snr':
-            val = compute_snr(val0, val1, *args, **kwargs)
-        elif metric == 'range':
-            val = compute_range(val0)
-        elif metric == 'max':
-            val = compute_max(val0)
-        elif metric == 'min':
-            val = compute_min(val0)
-        elif metric == 'mean':
-            val = compute_mean(val0)
-        elif metric == 'dtype':
-            return val0.dtype
-        else:
-            raise NotImplementedError
-        if val.nelement == 1:
-            return val.item()
-        return val.detach().cpu().numpy().tolist()
-
-    stat = []
-    for name in (recorder0.keys() & recorder1.keys()):
-        val0 = recorder0.get(name, None)
-        val1 = recorder1.get(name, None)
-        if val0 is None or val1 is None:
-            continue
-
-        tmp = {metric: compute(val0, val1, metric, *args, **kwargs)
-               for metric in metrics}
-        tmp['field'] = name
-        stat.append(tmp)
-
-    return stat
-
-
-def print_stats(stats, sorted_metric=None, reversed_order=None):
-    stats = copy.deepcopy(stats)
-    for stat in stats:
-        if sorted_metric is not None and reversed_order is not None:
-            stat = sorted(
-                stat, key=lambda x: x[sorted_metric], reverse=reversed_order)
-        for tensor_stat in stat:
-            name = tensor_stat.pop('field')
-            print(name, tensor_stat)
+def compute_metrics(real, pred, metrics=['cosine', 'snr', 'mse'], *args, **kwargs):
+    def inner_kernel(real, pred):
+        metric2func = {
+            'cosine': compute_cosine,
+            'snr': compute_snr,
+            'mse': compute_mse
+        }
+        return {
+            m: metric2func[m](real, pred, *args, **kwargs)
+            for m in metrics
+        }
+    return do_compute(real, pred, inner_kernel)
