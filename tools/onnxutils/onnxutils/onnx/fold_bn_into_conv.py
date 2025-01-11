207a9c1f773a9f4dd3a717fa6a60e7e161434eb9
ycgao <yc_x@outlook.com>
2025-01-11 18:20:52 +0800

    update fuse bn into conv

--- tools/onnxutils/onnxutils/onnx/fold_bn_into_conv.py
+++ tools/onnxutils/onnxutils/onnx/fold_bn_into_conv.py
@@ -1,23 +1,83 @@
-import os
-import warnings
-import tempfile
-
+import onnx
+import numpy as np
 
 from .onnx_model import OnnxModel
 
 from .registry import optimizer
+from .dag_matcher import DagMatcher
+
+dag_pattern = DagMatcher({
+    'id': 0,
+    'op_type': 'BatchNormalization',
+    'inputs': [
+        {
+            'id': 1,
+            'op_type': 'Conv'
+        },
+    ]
+})
 
 
 @optimizer('fold-bn-into-conv')
 class FoldConstant:
     @staticmethod
     def apply(onnx_model: OnnxModel) -> OnnxModel:
-        with tempfile.TemporaryDirectory() as workdir:
-            origin_model = os.path.join(workdir, 'origin.onnx')
-            output_model = os.path.join(workdir, 'output.onnx')
-            onnx_model.save(origin_model)
-            if not os.system(
-                    f"python3 -m onnxoptimizer {origin_model} {output_model}"):
-                return OnnxModel.from_file(output_model)
-            warnings.warn("'fuse-bn-into-conv' run failed")
+        with onnx_model.session() as sess:
+            for dag in dag_pattern.MatchAllDags(onnx_model):
+                bn_node = dag_pattern.GetNode(dag, 0)
+                conv_node = dag_pattern.GetNode(dag, 1)
+
+                epsilon = bn_node.attributes().get('epsilon', 1e-5)
+
+                bn_scale = onnx_model.get_initializer_by_name(
+                    bn_node.inputs()[1]).to_numpy()
+                bn_bias = onnx_model.get_initializer_by_name(
+                    bn_node.inputs()[2]).to_numpy()
+                bn_mean = onnx_model.get_initializer_by_name(
+                    bn_node.inputs()[3]).to_numpy()
+                bn_var = onnx_model.get_initializer_by_name(
+                    bn_node.inputs()[4]).to_numpy()
+
+                weight_factor = bn_scale / np.sqrt(bn_var + epsilon)
+                bias_factor = bn_bias - bn_scale * \
+                    bn_mean / np.sqrt(bn_var + epsilon)
+
+                conv_weight = onnx_model.get_initializer_by_name(
+                    conv_node.inputs()[1])
+                sess.remove_initializer(conv_weight)
+                conv_weight = conv_weight.to_numpy()
+                conv_weight = conv_weight * \
+                    weight_factor.reshape(
+                        *weight_factor.shape,
+                        *([1] * (conv_weight.ndim - weight_factor.ndim))
+                    )
+                sess.add_initializer(onnx.numpy_helper.from_array(
+                    conv_weight,
+                    conv_node.inputs()[1]))
+
+                if len(conv_node.inputs()) >= 3:
+                    conv_bias = onnx_model.get_initializer_by_name(
+                        conv_node.inputs()[2])
+                    sess.remove_initializer(conv_bias)
+
+                    conv_bias = conv_bias.to_numpy() + bias_factor
+                    sess.add_initializer(
+                        onnx.numpy_helper.from_array(
+                            conv_bias,
+                            conv_node.inputs()[2]
+                        )
+                    )
+                else:
+                    conv_bias = bias_factor
+                    sess.remove_node(conv_node)
+                    new_conv_node = conv_node.clone().proto()
+                    new_conv_node.input.append(sess.unique_name())
+                    sess.add_node(new_conv_node)
+                    sess.add_initializer(
+                        onnx.numpy_helper.from_array(
+                            conv_bias,
+                            new_conv_node.input[2]
+                        )
+                    )
+
             return onnx_model
