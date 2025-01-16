4a31c011d8d964fee4ef86141e3456f868ce5a4d
yinchaogao <yinchaogao@deeproute.ai>
2025-01-16 14:26:24 +0800

    update raid6

--- learn-by-demo/onnx-demo/torch2onnx.py
+++ learn-by-demo/onnx-demo/torch2onnx.py
@@ -17,7 +17,12 @@ def main():
     model = models.resnet50(models.ResNet50_Weights.IMAGENET1K_V2)
     torch_input = (torch.randn(1, 3, 224, 224), )
     torch.onnx.export(
-        model, torch_input, args.output, input_names=["data"], dynamic_axes={'data': {0: 'N'}})
+        model,
+        torch_input,
+        args.output,
+        input_names=["data"],
+        output_names=["y"]
+    )
 
 
 if __name__ == '__main__':
