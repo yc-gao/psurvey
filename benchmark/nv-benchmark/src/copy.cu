d12bf2cd2d2084fe5d21e98ad7d3f91eadeb58e6
ycgao <yc_x@outlook.com>
2025-07-26 01:30:02 +0800

    update

--- benchmark/nv-benchmark/src/copy.cu
+++ benchmark/nv-benchmark/src/copy.cu
@@ -1,41 +1,62 @@
 #include <cstddef>
+#include <cstdint>
+
 #include <nvbench/nvbench.cuh>
+#include <thrust/device_vector.h>
+
+void cuda_copy_bench(nvbench::state &state) {
+  const auto mem_size = state.get_int64("MemSize");
+
+  thrust::device_vector<std::int8_t> src_vec(mem_size);
+  thrust::device_vector<std::int8_t> dst_vec(mem_size);
+
+  state.add_element_count(mem_size);
+  state.add_global_memory_reads<std::int8_t>(mem_size);
+  state.add_global_memory_writes<std::int8_t>(mem_size);
+
+  state.exec([&](nvbench::launch &launch) {
+    cudaMemcpyAsync(thrust::raw_pointer_cast(dst_vec.data()),
+                    thrust::raw_pointer_cast(src_vec.data()), mem_size,
+                    cudaMemcpyDeviceToDevice, launch.get_stream());
+  });
+}
+NVBENCH_BENCH(cuda_copy_bench)
+    .add_int64_axis("MemSize", {1ul << 10, 1ul << 20, 1ul << 30});
 
 template <typename T>
-__global__ void copy_kernel(T *dst, const T *src, std::size_t len) {
-  for (auto i = threadIdx.x + blockIdx.x * blockDim.x; i < len;
-       i += blockDim.x * gridDim.x) {
-    dst[i] = src[i];
+__global__ void kernel_copy_bench_kernel(T *dst, T const *src, std::size_t n) {
+  for (auto idx = threadIdx.x + blockIdx.x * blockDim.x; idx < n;
+       idx += gridDim.x * blockDim.x) {
+    dst[idx] = src[idx];
   }
 }
-
-template <typename ValueType>
-void copy_bench(nvbench::state &state, nvbench::type_list<ValueType>) {
-  const auto mem_size =
-      state.get_int64("MemSize") / sizeof(ValueType) * sizeof(ValueType);
+template <typename T>
+void kernel_copy_bench(nvbench::state &state, nvbench::type_list<T>) {
+  const auto mem_size = state.get_int64("MemSize");
+  const auto grid_size = state.get_int64("GridSize");
   const auto block_size = state.get_int64("BlockSize");
-  const auto num_blocks = state.get_int64("NumBlocks");
 
-  state.add_global_memory_reads<nvbench::uint8_t>(mem_size);
-  state.add_global_memory_writes<nvbench::uint8_t>(mem_size);
-  state.collect_cupti_metrics();
+  const auto elem_count = mem_size / sizeof(T);
 
-  ValueType *dst, *src;
-  cudaMalloc(&dst, mem_size);
-  cudaMalloc(&src, mem_size);
+  thrust::device_vector<std::int8_t> src_vec(mem_size);
+  thrust::device_vector<std::int8_t> dst_vec(mem_size);
 
-  state.exec([=](nvbench::launch &launch) {
-    copy_kernel<<<num_blocks, block_size, 0, launch.get_stream()>>>(
-        dst, src, mem_size / sizeof(ValueType));
+  state.add_element_count(elem_count);
+  state.add_global_memory_reads<T>(elem_count);
+  state.add_global_memory_writes<T>(elem_count);
+
+  state.exec([&](nvbench::launch &launch) {
+    kernel_copy_bench_kernel<<<grid_size, block_size, 0, launch.get_stream()>>>(
+            reinterpret_cast<T*>(thrust::raw_pointer_cast(dst_vec.data())),
+            reinterpret_cast<T const *>(thrust::raw_pointer_cast(src_vec.data())),
+            elem_count);
   });
-  cudaFree(dst);
-  cudaFree(src);
 }
 NVBENCH_BENCH_TYPES(
-    copy_bench,
+    kernel_copy_bench,
     NVBENCH_TYPE_AXES(
-        nvbench::type_list<nvbench::uint8_t, nvbench::uint16_t,
-                           nvbench::uint32_t, nvbench::uint64_t, int4>))
-    .add_int64_power_of_two_axis("MemSize", nvbench::range(10, 30, 10))
-    .add_int64_power_of_two_axis("BlockSize", nvbench::range(5, 10, 1))
-    .add_int64_power_of_two_axis("NumBlocks", nvbench::range(5, 10, 2));
+        nvbench::type_list<std::int8_t, std::int16_t, std::int32_t, std::int64_t, int4>))
+    .add_int64_axis("MemSize", {1ul << 10, 1ul << 20, 1ul << 30})
+    .add_int64_axis("GridSize", {128, 512, 1024})
+    .add_int64_axis("BlockSize", {128, 512, 1024});
+
