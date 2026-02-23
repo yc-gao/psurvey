95e79d9c9d219ae6efdd887fbd602c8a1befc84e
ycgao <yc_x@outlook.com>
2026-02-23 05:48:20 +0000

    update

--- learn-by-demo/nextjs-demo/src/app/page.tsx
+++ learn-by-demo/nextjs-demo/src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Page() {
-  return (
-    <div>
-      hello world
+export default function Home() {
+  return <>
+    {/* nav bar */}
+    <div className="container mx-auto p-4 flex justify-between items-center">
+      <div>demo0</div>
+      <div>demo1</div>
     </div>
-  );
+  </>
 }
