903b806aa086b81308420bc61fe6168d06f65f46
ycgao <yc_x@outlook.com>
2026-03-02 03:29:54 +0000

    update

--- learn-by-demo/nextjs-demo/src/app/page.tsx
+++ learn-by-demo/nextjs-demo/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { BeakerIcon } from '@heroicons/react/24/solid'
 import { Button } from '@/components/ui/Button'
 
 export default function Page() {
@@ -5,5 +6,6 @@ export default function Page() {
     <Button>button default</Button>
     <Button variant="primary">button primary</Button>
     <Button variant="secondary" size="lg">button secondary</Button>
+    <BeakerIcon className="size-6 text-blue-500" />
   </div>
 }
