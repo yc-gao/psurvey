c5e68df6f48a84ff3cf3083e264dc7a92317cc8d
ycgao <yc_x@outlook.com>
2026-03-02 03:37:11 +0000

    update

--- learn-by-demo/nextjs-demo/src/app/layout.tsx
+++ learn-by-demo/nextjs-demo/src/app/layout.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import type { Metadata } from 'next'
 
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Next.js Demo",
+  description: "A demo of Next.js with Tailwind CSS and Heroicons",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
